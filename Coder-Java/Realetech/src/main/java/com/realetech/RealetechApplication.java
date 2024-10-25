1bbeaf08bb3d1814d38cbf21b56e20b3a80c04da
Alejandro A <alba.alej@gmail.com>
2024-10-25 11:36:14 -0300

    ahora si agrego realetech

---
+++ Coder-Java/Realetech/src/main/java/com/realetech/RealetechApplication.java
@@ -0,0 +1,13 @@
+package com.realetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealetechApplication.class, args);
+	}
+
+}
---
+++ Coder-Java/Realetech/src/test/java/com/realetech/RealetechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
