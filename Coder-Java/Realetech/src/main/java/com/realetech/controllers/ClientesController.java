d40f2d7ed99282b08b322433b85c2efc6a3715f5
Alejandro A <alba.alej@gmail.com>
2024-11-11 23:42:49 -0300

    asd

--- Coder-Java/Realetech/src/main/java/com/realetech/controllers/ClientesController.java
+++ Coder-Java/Realetech/src/main/java/com/realetech/controllers/ClientesController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +42,16 @@ public class ClientesController {
 	 		return clienteRepository.save(cliente);
 	 	}
 	 	
+	 	@PutMapping("/{id}")														// Modificar cliente
+	     public ResponseEntity<Cliente> updateClienteById(@PathVariable Long id, @RequestBody Cliente clienteDetails) {
+	         try {
+	             Cliente updatedCliente = clienteService.updateCliente(id, clienteDetails);
+	             return ResponseEntity.ok(updatedCliente);
+	         } catch (IllegalArgumentException e) {
+	 			return ResponseEntity.notFound().build();
+	         } catch (Exception e) {
+	             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	         }
+	     }
 }
 	 
